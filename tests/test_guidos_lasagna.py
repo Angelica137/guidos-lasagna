c89b4d31895c6ef4c025bd88a23b6f5bcb2ed2b8
Angelica <Angelica137@users.noreply.github.com>
2022-01-21 21:53:39 +0000

    .bake_time_remaining(), red

--- tests/test_guidos_lasagna.py
+++ tests/test_guidos_lasagna.py
@@ -4,3 +4,8 @@ from scripts.guidos_lasagna import *
 def test_expected_baske_time_constant_returns_40():
     lasagna = Lasagna()
     assert lasagna.EXPECTED_BAKE_TIME == 40
+
+
+def test_bake_time_remaining_returs_10():
+    l = Lasagna()
+    assert l.bake_time_remaining(30) == 10
