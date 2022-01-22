090f6ae1d9d1406d78fed950036500c830883578
Angelica <Angelica137@users.noreply.github.com>
2022-01-22 19:13:25 +0000

    test .elapsed_time_in_minutes(), green

--- scripts/guidos_lasagna.py
+++ scripts/guidos_lasagna.py
@@ -11,3 +11,13 @@ class Lasagna:
         Each layer takes 2 minutes to prepare.
         '''
         return layers*2
+
+    def elapsed_time_in_minutes(self, number_of_layers: int, elapsed_bake_time: int) -> int:
+        '''
+        Takes two parameters:
+        `number_of_layers` (_the number of layers added to the lasagna_); 
+        `elapsed_bake_time` (_the number of minutes the lasagna has been baking
+        in the oven_). 
+        Returns the total number of minutes user has been cooking.
+        '''
+        return elapsed_bake_time + self.preparation_time_in_minutes(number_of_layers)
