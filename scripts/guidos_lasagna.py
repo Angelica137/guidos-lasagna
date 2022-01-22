803d527fd09fb58b02e8401eea414fc91927e814
Angelica <Angelica137@users.noreply.github.com>
2022-01-22 18:53:53 +0000

    test .preparation_time_in_minutes(), green

--- scripts/guidos_lasagna.py
+++ scripts/guidos_lasagna.py
@@ -3,3 +3,11 @@ class Lasagna:
 
     def bake_time_remaining(self, time):
         return self.EXPECTED_BAKE_TIME - time
+
+    def preparation_time_in_minutes(self, layers: int) -> int:
+        '''
+        Takes the number of layers user wants to add to the lasagna as an argument
+        and returns how many minutes they would spend making them.
+        Each layer takes 2 minutes to prepare.
+        '''
+        return layers*2
