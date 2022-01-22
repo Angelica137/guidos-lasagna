6a75b5967b08c04d0aba6e4edf88959488ba1270
Angelica <Angelica137@users.noreply.github.com>
2022-01-22 18:49:23 +0000

    test .preparation_time_in_minutes(), red

--- tests/test_guidos_lasagna.py
+++ tests/test_guidos_lasagna.py
@@ -9,3 +9,8 @@ def test_expected_baske_time_constant_returns_40():
 def test_bake_time_remaining_returs_10():
     l = Lasagna()
     assert l.bake_time_remaining(30) == 10
+
+
+def test_preparation_time_in_minutes_returns_4():
+    l = Lasagna()
+    assert l.preparation_time_in_minutes(2) == 4
